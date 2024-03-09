ca00f35a26788314c58e6d41f22d9bd3a231e875
MaurrouViff <rofldu08@gmail.com>
2024-03-09 11:47:44 +0100

    Ajout de nombreuses choses

---
+++ controleur/meilleurResto.php
@@ -0,0 +1,23 @@
+<?php
+if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
+    $racine="..";
+}
+include_once "$racine/modele/bd.resto.inc.php";
+include_once "$racine/modele/bd.photo.inc.php";
+include_once "$racine/modele/bd.critiquer.inc.php";
+// recuperation des donnees GET, POST, et SESSION
+
+
+// appel des fonctions permettant de recuperer les donnees utiles a l'affichage
+$listeRestos = getRestos();
+$bestRestos = getRestosByNote();
+
+
+// traitement si necessaire des donnees recuperees
+;
+
+// appel du script de vue qui permet de gerer l'affichage des donnees
+$titre = "Les 4 meilleurs restaurants";
+include "$racine/vue/entete.html.php";
+include "$racine/vue/vueMeilleurResto.php";
+include "$racine/vue/pied.html.php";
